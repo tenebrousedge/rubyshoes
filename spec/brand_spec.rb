ee66a7275c13af19e9689c78eef3a2af9f8a759f
Patrick Leahy <tenebrousedge@gmail.com>
2017-05-12 16:35:14 -0700

    Add spec for after_find callback

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe Rubyshoes::Brand, type: :model do
   it { should have_and_belong_to_many :stores }
   it { is_expected.to callback(:capitalize_name).before :save }
+  it { is_expected.to callback(:price_to_s).after :find }
   it { should validate_uniqueness_of(:name) }
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_most(100) }
