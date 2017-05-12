d19b1d98e093a0f1f67e2a3ede6a95eadcf11130
Patrick Leahy <tenebrousedge@gmail.com>
2017-05-12 16:27:52 -0700

    Add specs to test model validation

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -3,4 +3,7 @@ require 'spec_helper'
 RSpec.describe Rubyshoes::Store, type: :model do
   it { should have_and_belong_to_many :brands }
   it { is_expected.to callback(:capitalize_name).before :save }
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_most(100) }
 end
