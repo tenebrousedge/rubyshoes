05d0d281ef9e5fea7e0e3f74d8ba55bac3044908
Patrick Leahy <tenebrousedge@gmail.com>
2017-05-12 19:10:38 -0700

    update stores controller with slightly different interface

--- controllers/stores_controller.rb
+++ controllers/stores_controller.rb
@@ -12,17 +12,20 @@ class StoresController < Sinatra::Base
   end
 
   get '/stores/new' do
+    @brands = Rubyshoes::Brand.all
     haml :new_store
   end
 
   get '/stores/:id' do |id|
+    @brands = Rubyshoes::Brand.all
     @store = M.find(id)
     haml :store
   end
 
   post '/stores' do
     new_store = params.fetch(:store)
-    M.create(new_store)
+    s = M.create(new_store)
+    s.brands << params.fetch(:brand)
     @stores = M.all
     haml :stores
   end
@@ -30,6 +33,7 @@ class StoresController < Sinatra::Base
   patch '/stores/:id' do |id|
     new_data = params.fetch(:store)
     @store = M.find(id).update(new_data)
+    @store.brands << params.fetch(:brand)
     redirect_back
   end
 
